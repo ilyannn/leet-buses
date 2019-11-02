f3d66addde6932a19835022102dd3c2c62ea4f7f
Ilya Nikokoshev <ilya.nikokoshev@gmail.com>
2019-11-02 22:37:48 +0300

    Second solution

--- buses.py
+++ buses.py
@@ -9,23 +9,32 @@ class Solution(object):
         :type T: int
         :rtype: int
         """
-        direct = defaultdict(set)
-        for route in routes:
+        if S == T:
+            return 0
+
+        routes_at = defaultdict(set)
+        for route_id, route in enumerate(routes):
             for stop in route:
-                direct[stop].update(route)
+                routes_at[stop].add(route_id)
+
+        edges = defaultdict(set)
+        for route_set in routes_at.values():
+            for a in route_set:
+                edges[a].update(route_set)
 
         previous = set()
-        current = set([S])
-        step = 0
+        current = routes_at[S]
+        target = routes_at[T]
+        step = 1
 
-        while T not in current:
+        while current.isdisjoint(target):
             if len(current) == len(previous):
                 return -1
 
-            step += 1
             new_elements = current.difference(previous)
             previous = current.copy()
             for stop in new_elements:
-                current.update(direct[stop])
+                current.update(edges[stop])
+            step += 1
 
         return step
