63d83c22d75a6ce8a1ebe65234ca68192b11352c
Ilya Nikokoshev <ilya.nikokoshev@gmail.com>
2019-11-02 22:52:16 +0300

    More tests and comments

--- buses.py
+++ buses.py
@@ -1,7 +1,31 @@
 from collections import defaultdict
 
 
+def prepareGraph(routes, S, T):
+    """ Returns the edges of the graph as well as the source and target sets
+    """
+    # Which bus routes go through this stop?
+    routes_at = defaultdict(set)
+
+    # We'll use `enumerate` to assign each bus an id in order (0, 1, ...)
+    for route_id, route in enumerate(routes):
+        for stop in route:
+            # And fill `routes_at` for each stop.
+            routes_at[stop].add(route_id)
+
+    # What are the edges of our graph?
+    edges = defaultdict(set)
+    for route_set in routes_at.values():
+        # Each stop gives an edge between all routes that pass through it.
+        for a in route_set:
+            edges[a].update(route_set)
+
+    # Now return the edges, source and target sets.
+    return edges, routes_at[S], routes_at[T]
+
+
 class Solution(object):
+
     def numBusesToDestination(self, routes, S, T):
         """
         :type routes: List[List[int]]
@@ -9,24 +33,19 @@ class Solution(object):
         :type T: int
         :rtype: int
         """
+
+        # Short-circuit the obvious case.
         if S == T:
             return 0
 
-        routes_at = defaultdict(set)
-        for route_id, route in enumerate(routes):
-            for stop in route:
-                routes_at[stop].add(route_id)
+        # Let's treat the buses as nodes of the graph.
+        edges, current, target = prepareGraph(routes, S, T)
 
-        edges = defaultdict(set)
-        for route_set in routes_at.values():
-            for a in route_set:
-                edges[a].update(route_set)
-
+        # Now we can use the search for the shortest path.
         previous = set()
-        current = routes_at[S]
-        target = routes_at[T]
         step = 1
 
+        # We'll stop when we reach one of the target buses.
         while current.isdisjoint(target):
             if len(current) == len(previous):
                 return -1
@@ -37,4 +56,5 @@ class Solution(object):
                 current.update(edges[stop])
             step += 1
 
+        # Found it!
         return step
--- test_buses.py
+++ test_buses.py
@@ -1,20 +1,25 @@
 import unittest
 from buses import Solution
 
+routes = [[1, 2, 7], [3, 6, 7]]
+S = 1
+
+
 class MyTestCase(unittest.TestCase):
     def test_3routes(self):
-        routes = [[1, 2, 7], [3, 6, 7]]
-        S = 1
         T = 6
         answer = Solution().numBusesToDestination(routes, S, T)
         self.assertEqual(answer, 2)
 
     def test_not_possible(self):
-        routes = [[1, 2, 7], [3, 6, 7]]
-        S = 1
         T = 4
         answer = Solution().numBusesToDestination(routes, S, T)
         self.assertEqual(answer, -1)
+
+    def test_same_stop(self):
+        T = 1
+        answer = Solution().numBusesToDestination(routes, S, T)
+        self.assertEqual(answer, 0)
 
 
 if __name__ == '__main__':
